e6da86845dafaedc5d017f97a588e2957ec68275
Albert-Byrone <albertbyrone1677@gmail.com>
2021-08-05 22:56:12 +0300

    monir changes

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.testapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var click_btn = findViewById(R.id.button) as Button
+
+        click_btn.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
